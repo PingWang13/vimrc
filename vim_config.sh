c167918f7ac61f193f85770fb03296ccc28d1447
PingWang13 <drivingforce@hotmail.com>
2018-05-08 11:36:03 +0800

    初始化提交

---
+++ vim_config.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#sudo apt-get install git
+#sudo git clone https://github.com/PingWang13/vimrc.git   ~/package
+sudo apt-get install ctags
+sudo cp -rf ~/package/vimrc  ~/.vimrc
+sudo mkdir -p ~/.vim/colors
+sudo cp -rf ~/package/molokai.vim    ~/.vim/colors/
+sudo git clone https://github.com/gmarik/vundle.git  ~/.vim/bundle/vundle
+sudo vim +BundleList +BundleInstall +qall
+
