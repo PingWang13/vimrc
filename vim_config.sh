bb3d1f9a4f7062a75820ab5a3ae12e70e1d15bd0
PingWang13 <37463769+PingWang13@users.noreply.github.com>
2018-05-08 11:32:00 +0800

    Delete vim_config.sh

--- vim_config.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo apt-get install ctags
-sudo cp -rf ~/package/vimrc  ~/.vimrc
-sudo mkdir -p ~/.vim/colors
-sudo cp -rf ~/package/molokai.vim    ~/.vim/colors/
-sudo git clone https://github.com/gmarik/vundle.git  ~/.vim/bundle/vundle
-sudo vim +BundleList +BundleInstall +qall
