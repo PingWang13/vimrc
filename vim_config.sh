c56f6bb8b83688ba5606f86fbceb6cce479a9e5c
PingWang13 <37463769+PingWang13@users.noreply.github.com>
2018-05-13 12:48:40 +0800

    Update vim_config.sh

--- vim_config.sh
+++ vim_config.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 #sudo apt-get install git
 #sudo git clone https://github.com/PingWang13/vimrc.git   ~/package
+sudo apt-get update
 sudo apt-get install ctags
+sudo apt-get update
+sudo apt-get install vim
 sudo cp -rf ~/package/vimrc  ~/.vimrc
 sudo mkdir -p ~/.vim/colors
 sudo cp -rf ~/package/molokai.vim    ~/.vim/colors/
